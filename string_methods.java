552f62bcbf8c1bb4ada74833f9d0010d4122e279
VENKATA NAGA SAI SUCHARITH.M <90145299+msaisucharith@users.noreply.github.com>
2023-03-12 20:39:38 +0530

    Add files via upload

---
+++ string_methods.java
@@ -0,0 +1,20 @@
+public class string_methods {
+    public static void main(String args[]){
+        String txt="javaString";
+        String s1="sai";
+        String s2="sucharith";
+
+
+//        for finding length
+        System.out.println("the length of string "+txt.length());
+
+//        changing to uppercase
+        System.out.println("To upper case : "+txt.toUpperCase());
+
+//        to  find index
+        System.out.println("java index :"+txt.indexOf("St"));
+
+//        concatenation using concat
+        System.out.println(s1.concat(s2));
+    }
+}
