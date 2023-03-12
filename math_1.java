cc7c4358bbca8eb47c7d31a8d36e8ccf9d77e764
VENKATA NAGA SAI SUCHARITH.M <90145299+msaisucharith@users.noreply.github.com>
2023-03-12 21:12:01 +0530

    Add files via upload

---
+++ math_1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class math_1
+{
+    public static void main(String args[]){
+
+        int in;
+        in=new Scanner(System.in).nextInt();
+
+        System.out.println(in);
+
+//        using math
+        System.out.println(Math.max(10,1000));
+
+        System.out.println(Math.min(0,-9));
+
+        System.out.println(Math.ceil(90.8));
+    }
+}
