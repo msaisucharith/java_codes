ee3ce47182bdb4983fa6b59e5e323ea5036c89cc
VENKATA NAGA SAI SUCHARITH.M <90145299+msaisucharith@users.noreply.github.com>
2023-03-13 20:57:27 +0530

    Add files via upload

---
+++ fibonacci_series.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class fibonacci_series {
+    static void Fibonacci(int N)
+    {
+        int num1 = 0, num2 = 1;
+
+        int counter = 0;
+
+        while (counter < N) {
+
+            System.out.print(num1 + " ");
+
+            int num3=num1+num2;
+            num1=num2;
+            num2=num3;
+            counter=counter+1;
+        }
+    }
+
+    public static void main(String args[])
+    {
+        int N = 10;
+
+        Fibonacci(N);
+    }
+}
