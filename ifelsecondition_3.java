e06fbf420d4ce6be81e7eb7e2f8df3f56bc20376
VENKATA NAGA SAI SUCHARITH.M <90145299+msaisucharith@users.noreply.github.com>
2023-03-13 20:32:24 +0530

    Add files via upload

---
+++ ifelsecondition_3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ifelsecondition_3 {
+
+    public static void main(String args[]){
+        int input;
+        input=new Scanner(System.in).nextInt();
+
+        if(input<=10){
+            System.out.println("number is below 10")
+            ;
+        } else if (input>=10) {
+            System.out.println("number is grater than 10");
+
+        }
+    }
+}
