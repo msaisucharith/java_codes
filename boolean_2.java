e9a5ecffcff27ea5decc27c4643c97fda9c47dac
VENKATA NAGA SAI SUCHARITH.M <90145299+msaisucharith@users.noreply.github.com>
2023-03-13 20:23:22 +0530

    Add files via upload

---
+++ boolean_2.java
@@ -0,0 +1,21 @@
+public class boolean_2 {
+    public static void main(String args[]){
+
+//        for boolean we need to inti the boolean data type
+        boolean isTrue=true;
+        int myAge=23;
+        int yourAge=34;
+        System.out.println(isTrue);
+
+//        we can check the condition like equal or not
+        System.out.println(10>9);
+//        it will return true
+
+        if(myAge>=yourAge){
+            System.out.println("my age is grater");
+        }else {
+            System.out.println("your age is higher");
+        }
+
+    }
+}
